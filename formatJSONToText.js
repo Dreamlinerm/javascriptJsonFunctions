2ec8336525d322ef1803320162e13fa0def59607
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-04-13 12:46:03 +0200

    minor text fix

--- formatJSONToText.js
+++ formatJSONToText.js
@@ -5,9 +5,13 @@ function formatJSONToText(data, path = "") {
   if (typeof data != "object" || data == null)
     formattedData += `$ ${path.slice(0, -1)}\n${data}\n\n`;
   else if (typeof data === "object") {
-    Object.keys(data).forEach((key) => {
-      formattedData += formatJSONToText(data[key], `${path + key + "."}`);
-    });
+    if (Object.keys(data).length > 0) {
+      Object.keys(data).forEach((key) => {
+        formattedData += formatJSONToText(data[key], `${path + key + "."}`);
+      });
+    } else {
+      formattedData += `${path.slice(0, -1)}\n${JSON.stringify(data)}\n\n`;
+    }
   }
   return formattedData;
 }
